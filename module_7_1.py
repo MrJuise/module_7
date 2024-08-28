1195befc1ba23d5721559ff077586502d8abfb89
MrJuise <126503964+MrJuise@users.noreply.github.com>
2024-08-28 19:30:43 +0300

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,41 @@
+'''Задача "Учёт товаров":
+Необходимо реализовать 2 класса Product и Shop, с помощью которых будет производиться запись в файл с продуктами.
+Объекты класса Product будут создаваться следующим образом - Product('Potato', 50.0, 'Vagetables') и обладать следующими свойствами:'
+'''
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = str(name)
+        self.weight = float(weight)
+        self.category = str(category)
+
+    def __str__(self):
+        return (f'{self.name}, {self.weight}, {self.category}')
+
+class Shop:
+    __file_name = 'products.txt'
+    def get_products(self):
+        with open(self.__file_name, 'r') as file:
+            return file.read()
+
+    def add(self, *products):
+        for p in products:
+            if p.name in self.get_products():
+                print(f'Продукт {p.name} уже есть в магазине')
+            else:
+                file = open(self.__file_name, 'a')
+                file.write(f'{p}\n')
+                file.close()
+
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
